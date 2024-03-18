67e22ff696c92517bfcfc1ba63c2e2e1229a1d2f
la46587 <la46587@zut.edu.pl>
2024-03-18 22:21:14 +0100

    lab04 in progress
    czaicie ze 3 godziny siedzialem nad dyderingiem losowym xddd nie polecam pzdr widzowie

--- lab04/4.py
+++ lab04/4.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 
 def imgToFloat(img):
@@ -8,6 +9,7 @@ def imgToFloat(img):
         return img
     else:
         img = img / 255.0
+        img = img[:, :, :3]
         return img
 
 
@@ -23,33 +25,110 @@ def colorFit(pixel, palette):
 
 def kwantColorFit(img, palette):
     outImg = img.copy()
-    height = img.shape[0]
-    width = img.shape[1]
-    for w in range(height):
-        for k in range(width):
+    for w in range(img.shape[0]):
+        for k in range(img.shape[1]):
             outImg[w, k] = colorFit(img[w, k], palette)
     return outImg
 
-fig, axs = plt.subplots(2, 2)
-fig.tight_layout()
 
-img = plt.imread('GS_0001.tif')
+def ditheringRandom(img):
+    r = np.random.rand(*img.shape)
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i, j][0] >= r[i, j][0]:
+                img[i, j] = 1
+            else:
+                img[i, j] = 0
+    return img
+
+
+def ditheringOrganized(img):
+    pass
+
+
+def ditheringFloydSteinberg(img):
+    pass
+
+
+palette8 = np.array([
+        [0.0, 0.0, 0.0,],
+        [0.0, 0.0, 1.0,],
+        [0.0, 1.0, 0.0,],
+        [0.0, 1.0, 1.0,],
+        [1.0, 0.0, 0.0,],
+        [1.0, 0.0, 1.0,],
+        [1.0, 1.0, 0.0,],
+        [1.0, 1.0, 1.0,],
+])
+
+palette16 = np.array([
+        [0.0, 0.0, 0.0,],
+        [0.0, 1.0, 1.0,],
+        [0.0, 0.0, 1.0,],
+        [1.0, 0.0, 1.0,],
+        [0.0, 0.5, 0.0,],
+        [0.5, 0.5, 0.5,],
+        [0.0, 1.0, 0.0,],
+        [0.5, 0.0, 0.0,],
+        [0.0, 0.0, 0.5,],
+        [0.5, 0.5, 0.0,],
+        [0.5, 0.0, 0.5,],
+        [1.0, 0.0, 0.0,],
+        [0.75, 0.75, 0.75,],
+        [0.0, 0.5, 0.5,],
+        [1.0, 1.0, 1.0,],
+        [1.0, 1.0, 0.0,]
+])
+
+# fig, axs = plt.subplots(2, 2)
+# fig.tight_layout()
+#
+# img = cv2.imread('GS_0002.png')
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# img = imgToFloat(img)
+#
+# axs[0, 0].imshow(img)
+# axs[0, 0].set_title('Original')
+#
+# palette = generatePalette(2)
+# img1 = kwantColorFit(img, palette)
+# axs[0, 1].imshow(img1)
+# axs[0, 1].set_title('1-bit')
+#
+# palette = generatePalette(4)
+# img2 = kwantColorFit(img, palette)
+# axs[1, 0].imshow(img2)
+# axs[1, 0].set_title('2-bit')
+#
+# palette = generatePalette(16)
+# img4 = kwantColorFit(img, palette)
+# axs[1, 1].imshow(img4)
+# axs[1, 1].set_title('4-bit')
+# plt.show()
+#
+# plt.close(fig)
+# img8 = kwantColorFit(img, palette8)
+# plt.imshow(img8)
+# plt.show()
+#
+# img16 = kwantColorFit(img, palette16)
+# plt.imshow(img16)
+# plt.show()
+
+img = cv2.imread('GS_0002.png')
 img = imgToFloat(img)
-axs[0, 0].imshow(img)
-axs[0, 0].set_title('Original')
+plt.imshow(img)
+plt.show()
 
-palette = generatePalette(2)
-img2 = kwantColorFit(img, palette)
-axs[0, 1].imshow(img2)
-axs[0, 1].set_title('1-bit')
+img1 = kwantColorFit(img, generatePalette(2))
+plt.imshow(img1)
+plt.show()
 
-palette = generatePalette(4)
-img4 = kwantColorFit(img, palette)
-axs[1, 0].imshow(img4)
-axs[1, 0].set_title('2-bit')
+imgDitherRandom = ditheringRandom(img)
+plt.imshow(imgDitherRandom)
+plt.show()
 
-palette = generatePalette(16)
-img16 = kwantColorFit(img, palette)
-axs[1, 1].imshow(img16)
-axs[1, 1].set_title('4-bit')
+imgDitherOrganized = ditheringOrganized(img)
+plt.imshow(imgDitherOrganized)
 plt.show()
