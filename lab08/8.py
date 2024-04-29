fbdc0c5ef01e2eaae9afd7360e4593c491e898d8
la46587 <la46587@zut.edu.pl>
2024-04-29 23:21:34 +0200

    lab08 done

--- lab08/8.py
+++ lab08/8.py
@@ -1,46 +1,11 @@
 from docx import Document
 from docx.shared import Inches
-from io import BytesIO
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import cv2
 from scipy.fftpack import dct, idct
-
-
-class JPEG:
-    def __init__(self, Y, Cb, Cr, shape, Ratio, QY, QC):
-        self.Y = Y
-        self.Cb = Cb
-        self.Cr = Cr
-        self.shape = shape
-        self.ChromaRatio = Ratio
-        self.QY = QY
-        self.QC = QC
-
-
-def CompressJPEG(image, ratio="4:4:4", QY=None, QC=None):
-    YCbCr = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
-    Y, Cb, Cr = YCbCr[:, :, 0], YCbCr[:, :, 1], YCbCr[:, :, 2]
-    Y, Cb, Cr = subsample(Y, Cb, Cr)
-
-    compressed_Y = compress_layer(Y, QY)
-    compressed_Cb = compress_layer(Cb, QC)
-    compressed_Cr = compress_layer(Cr, QC)
-
-    return {
-        'Y': compressed_Y, 'Cb': compressed_Cb, 'Cr': compressed_Cr,
-        'shape': Y.shape, 'ChromaRatio': ratio, 'QY': QY, 'QC': QC
-    }
-
-
-def DecompressJPEG(jpeg):
-    Y = decompress_layer(jpeg['Y'], jpeg['QY'], jpeg['shape'])
-    Cb = decompress_layer(jpeg['Cb'], jpeg['QC'], jpeg['shape'])
-    Cr = decompress_layer(jpeg['Cr'], jpeg['QC'], jpeg['shape'])
-
-    decompressed_YCbCr = np.stack((Y, Cb, Cr), axis=-1)
-    return cv2.cvtColor(decompressed_YCbCr.astype(np.uint8), cv2.COLOR_YCrCb2RGB)
+from io import BytesIO
 
 
 def dct2(a):
@@ -51,40 +16,79 @@ def idct2(a):
     return idct(idct(a.T, norm='ortho').T, norm='ortho')
 
 
-def compress_block(block, Q):
-    dct_block = dct2(block)
-    quantized_block = np.round(dct_block / Q).astype(int)
-    return quantized_block.flatten()
+class JPEGChromaSubsample:
+    def __init__(self, image, ratio):
+        self.image = image
+        self.ratio = ratio
+        self.YCbCr = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+        self.Y, self.Cb, self.Cr = self.YCbCr[:, :, 0], self.YCbCr[:, :, 1], self.YCbCr[:, :, 2]
+        self.subsample()
+
+    def subsample(self):
+        if self.ratio == "4:2:2":
+            self.Cb = self.Cb[:, ::2]
+            self.Cr = self.Cr[:, ::2]
+
+    def recover(self):
+        YCbCr = np.empty_like(self.YCbCr)
+        YCbCr[:, :, 0] = self.Y
+        YCbCr[:, :, 1] = np.repeat(self.Cb, 2, axis=1) if self.ratio == "4:2:2" else self.Cb
+        YCbCr[:, :, 2] = np.repeat(self.Cr, 2, axis=1) if self.ratio == "4:2:2" else self.Cr
+        return cv2.cvtColor(YCbCr, cv2.COLOR_YCrCb2RGB)
 
 
-def decompress_block(vector, Q):
-    reshaped_block = vector.reshape(8, 8)
-    dequantized_block = reshaped_block * Q
-    return idct2(dequantized_block)
+def process_channel(channel, Q):
+    height, width = channel.shape
+    padded_height = (height + 7) // 8 * 8
+    padded_width = (width + 7) // 8 * 8
+    padded_channel = np.zeros((padded_height, padded_width))
+    padded_channel[:height, :width] = channel
+
+    recovered_channel = np.zeros_like(padded_channel)
+
+    for i in range(0, padded_height, 8):
+        for j in range(0, padded_width, 8):
+            block = padded_channel[i:i + 8, j:j + 8]
+            dct_block = dct2(block)
+            quantized_block = np.round(dct_block / Q)
+            dequantized_block = quantized_block * Q
+            recovered_block = idct2(dequantized_block)
+            recovered_channel[i:i + 8, j:j + 8] = recovered_block
+
+    return recovered_channel[:height, :width]
 
 
-def compress_layer(L, Q):
-    S = np.array([])
-    for w in range(0, L.shape[0], 8):
-        for k in range(0, L.shape[1], 8):
-            block = L[w:(w + 8), k:(k + 8)]
-            S = np.append(S, compress_block(block, Q))
-    return S
+class JPEGQuantization:
+    def __init__(self, image, QY, QC):
+        self.image = image
+        if QY is None:
+            self.QY = np.ones((8, 8))
+        else:
+            self.QY = QY
+        if QC is None:
+            self.QC = np.ones((8, 8))
+        else:
+            self.QC = QC
+        self.YCbCr = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+        self.Y, self.Cb, self.Cr = self.YCbCr[:, :, 0], self.YCbCr[:, :, 1], self.YCbCr[:, :, 2]
+
+    def recover(self):
+        Y = process_channel(self.Y, self.QY)
+        Cb = process_channel(self.Cb, self.QC)
+        Cr = process_channel(self.Cr, self.QC)
+
+        YCbCr = np.stack((Y, Cb, Cr), axis=-1)
+        return cv2.cvtColor(YCbCr.astype(np.uint8), cv2.COLOR_YCrCb2RGB)
 
 
-def decompress_layer(S, Q, dims):
-    L = np.zeros(dims)
-    num_blocks_w = dims[1] // 8
-    for idx, i in enumerate(range(0, S.shape[0], 64)):
-        vector = S[i:(i + 64)]
-        w = (idx // num_blocks_w) * 8
-        k = (idx % num_blocks_w) * 8
-        L[w:(w + 8), k:(k + 8)] = decompress_block(vector, Q)
-    return L
+def calculateCompressionRatio(img, imgEncoded):
+    originalSize = img.size
+    encodedSize = len(imgEncoded)
 
+    compressionRatio = abs(originalSize) / abs(encodedSize)
+    compressionPercentage = 100 * (abs(encodedSize) / abs(originalSize))
 
-def subsample(Y, Cb, Cr):
-    return Y, Cb, Cr
+    return compressionRatio, compressionPercentage
 
 
 def encodeRLE(img):
@@ -102,16 +106,6 @@ def encodeRLE(img):
             count = 1
     encoded.append((count, prevPixel))
     return encoded
-
-
-def calculateCompressionRatio(img, imgEncoded):
-    originalSize = img.size
-    encodedSize = len(imgEncoded)
-
-    compressionRatio = abs(originalSize) / abs(encodedSize)
-    compressionPercentage = 100 * (abs(encodedSize) / abs(originalSize))
-
-    return compressionRatio, compressionPercentage
 
 
 QY = np.array([
@@ -139,14 +133,15 @@ QC = np.array([
 document = Document()
 document.add_heading('Laboratorium 08', 0)
 
-images = ['BIG_0001.jpg', 'BIG_0003.jpg', 'moon.jpg', 'colored.jpg']
+images = ['frag1_1.jpg', 'frag1_2.jpg', 'frag1_3.jpg', 'frag2_1.jpg', 'frag2_2.jpg', 'frag2_3.jpg', 'frag3_1.jpg',
+          'frag3_2.jpg', 'frag3_3.jpg', 'frag4_1.jpg', 'frag4_2.jpg', 'frag4_3.jpg']
 
 for image in images:
     memfile = BytesIO()
 
     input_image = mpimg.imread(image)
-    compressed_image = CompressJPEG(input_image, "4:4:4", QY, QC)
-    output_image = DecompressJPEG(compressed_image)
+    processed_image = JPEGChromaSubsample(input_image, "4:4:4")
+    output_image = processed_image.recover()
 
     imgEncodedRLE = encodeRLE(output_image.copy())
     compressionRatio, compressionPercentage = calculateCompressionRatio(output_image, imgEncodedRLE)
@@ -170,6 +165,106 @@ for image in images:
         axs[i + 1, 1].axis('off')
 
     plt.savefig(memfile, bbox_inches='tight')
+    document.add_heading('Redukcja chrominancji - 4:4:4', 1)
+    document.add_heading(f'{image} - stopień kompresji: {compressionRatio:.4f} ({compressionPercentage:.2f}%)', 2)
+    document.add_picture(memfile, width=Inches(4.3))
+    plt.close(fig)
+
+    memfile = BytesIO()
+
+    input_image = mpimg.imread(image)
+    processed_image = JPEGChromaSubsample(input_image, "4:2:2")
+    output_image = processed_image.recover()
+
+    imgEncodedRLE = encodeRLE(output_image.copy())
+    compressionRatio, compressionPercentage = calculateCompressionRatio(output_image, imgEncodedRLE)
+
+    fig, axs = plt.subplots(4, 2, sharey='all')
+    fig.set_size_inches(9, 13)
+    fig.tight_layout()
+
+    axs[0, 0].imshow(input_image)
+    axs[0, 0].axis('off')
+    axs[0, 1].imshow(output_image)
+    axs[0, 1].axis('off')
+
+    input_ycbcr = cv2.cvtColor(input_image, cv2.COLOR_RGB2YCrCb)
+    output_ycbcr = cv2.cvtColor(output_image, cv2.COLOR_RGB2YCrCb)
+
+    for i in range(3):
+        axs[i + 1, 0].imshow(input_ycbcr[:, :, i], cmap='gray')
+        axs[i + 1, 0].axis('off')
+        axs[i + 1, 1].imshow(output_ycbcr[:, :, i], cmap='gray')
+        axs[i + 1, 1].axis('off')
+
+    plt.savefig(memfile, bbox_inches='tight')
+    document.add_heading('Redukcja chrominancji - 4:2:2', 1)
+    document.add_heading(f'{image} - stopień kompresji: {compressionPercentage:.4f} ({compressionPercentage:.2f}%)', 2)
+    document.add_picture(memfile, width=Inches(4.3))
+    plt.close(fig)
+
+    memfile = BytesIO()
+
+    input_image = mpimg.imread(image)
+    processed_image = JPEGQuantization(input_image, QY, QC)
+    output_image = processed_image.recover()
+
+    imgEncodedRLE = encodeRLE(output_image.copy())
+    compressionRatio, compressionPercentage = calculateCompressionRatio(output_image, imgEncodedRLE)
+
+    fig, axs = plt.subplots(4, 2, sharey='all')
+    fig.set_size_inches(9, 13)
+    fig.tight_layout()
+
+    axs[0, 0].imshow(input_image)
+    axs[0, 0].axis('off')
+    axs[0, 1].imshow(output_image)
+    axs[0, 1].axis('off')
+
+    input_ycbcr = cv2.cvtColor(input_image, cv2.COLOR_RGB2YCrCb)
+    output_ycbcr = cv2.cvtColor(output_image, cv2.COLOR_RGB2YCrCb)
+
+    for i in range(3):
+        axs[i + 1, 0].imshow(input_ycbcr[:, :, i], cmap='gray')
+        axs[i + 1, 0].axis('off')
+        axs[i + 1, 1].imshow(output_ycbcr[:, :, i], cmap='gray')
+        axs[i + 1, 1].axis('off')
+
+    plt.savefig(memfile, bbox_inches='tight')
+    document.add_heading('Kwantyzacja - tablice kwantyzujące', 1)
+    document.add_heading(f'{image} - stopień kompresji: {compressionPercentage:.4f} ({compressionPercentage:.2f}%)', 2)
+    document.add_picture(memfile, width=Inches(4.3))
+    plt.close(fig)
+
+    memfile = BytesIO()
+
+    input_image = mpimg.imread(image)
+    processed_image = JPEGQuantization(input_image, None, None)
+    output_image = processed_image.recover()
+
+    imgEncodedRLE = encodeRLE(output_image.copy())
+    compressionRatio, compressionPercentage = calculateCompressionRatio(output_image, imgEncodedRLE)
+
+    fig, axs = plt.subplots(4, 2, sharey='all')
+    fig.set_size_inches(9, 13)
+    fig.tight_layout()
+
+    axs[0, 0].imshow(input_image)
+    axs[0, 0].axis('off')
+    axs[0, 1].imshow(output_image)
+    axs[0, 1].axis('off')
+
+    input_ycbcr = cv2.cvtColor(input_image, cv2.COLOR_RGB2YCrCb)
+    output_ycbcr = cv2.cvtColor(output_image, cv2.COLOR_RGB2YCrCb)
+
+    for i in range(3):
+        axs[i + 1, 0].imshow(input_ycbcr[:, :, i], cmap='gray')
+        axs[i + 1, 0].axis('off')
+        axs[i + 1, 1].imshow(output_ycbcr[:, :, i], cmap='gray')
+        axs[i + 1, 1].axis('off')
+
+    plt.savefig(memfile, bbox_inches='tight')
+    document.add_heading('Kwantyzacja - tablica jedynek', 1)
     document.add_heading(f'{image} - stopień kompresji: {compressionPercentage:.4f} ({compressionPercentage:.2f}%)', 2)
     document.add_picture(memfile, width=Inches(4.3))
     plt.close(fig)
